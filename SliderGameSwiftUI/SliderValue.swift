0bb114a12a19da539800c3172a0570423b645645
Aleksandr F <a7214370@gmail.com>
2022-05-25 02:09:41 +0300

    Setup SliderValue

---
+++ SliderGameSwiftUI/SliderValue.swift
@@ -0,0 +1,20 @@
+//
+//  SliderValue.swift
+//  SliderGameSwiftUI
+//
+//  Created by Aleksandr F. on 25.05.2022.
+//
+
+import SwiftUI
+
+struct SliderValue: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SliderValue_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderValue()
+    }
+}
