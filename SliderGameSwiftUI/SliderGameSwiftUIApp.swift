9cbc4d3b710be9414eb8ef226e0e6688cb6ef97a
Aleksandr F <a7214370@gmail.com>
2022-05-24 18:23:21 +0300

    Initial Commit

---
+++ SliderGameSwiftUI/SliderGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameSwiftUIApp.swift
+//  SliderGameSwiftUI
+//
+//  Created by Aleksandr F. on 24.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
