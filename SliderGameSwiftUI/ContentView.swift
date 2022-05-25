170524c9262a0a0e9fbdcd92990e2b7fd106a098
Aleksandr F <a7214370@gmail.com>
2022-05-25 05:04:50 +0300

    Work with ContentView

--- SliderGameSwiftUI/ContentView.swift
+++ SliderGameSwiftUI/ContentView.swift
@@ -8,8 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
-
-
+    
+    @State private var targetValue = Int.random(in: 0...100)
+    @State private var currentValue: Float = 50.0
+    @State private var alpha: CGFloat = 0
+    @State private var alert = false
+    
+    var body: some View {
+        VStack {
+            Text("Подвинь слайдер, как можно ближе к: \(targetValue)")
+                .padding()
+            
+            HStack{
+                Text("1")
+                SliderValue(sliderValue: $currentValue, alpha: $alpha)
+                Text("100")
+            }
+            .onChange(of: currentValue) {
+                value in currentValue = Float(Int(value))
+                alphaValue()
+            }
+            .onAppear { alphaValue() }
+            .padding()
+            
+            Button("Проверь меня!") {
+                alert.toggle()
+            }
+            .alert(isPresented: $alert) {
+                Alert(
+                    title: Text ("Your Score"),
+                    message: Text("\(computeScore())"),
+                    dismissButton: .cancel()
+                )
+            }
+            .padding()
+            Button("Начать заново") {
+                targetValue = Int.random(in: 1...100)
+                alphaValue()
+            }
+        }
+    }
+    
+    private func alphaValue() {
+        alpha = CGFloat(computeScore())
+    }
+    
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(Double(currentValue)))
+        return 100 - difference
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
