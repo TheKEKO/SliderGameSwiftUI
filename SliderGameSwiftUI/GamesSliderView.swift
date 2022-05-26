615be12f4f0058d4344249e86125b048791746a4
Aleksandr F <a7214370@gmail.com>
2022-05-26 11:20:16 +0300

    Setup GamesSliderView

---
+++ SliderGameSwiftUI/GamesSliderView.swift
@@ -0,0 +1,20 @@
+//
+//  GamesSliderView.swift
+//  SliderGameSwiftUI
+//
+//  Created by Aleksandr F. on 26.05.2022.
+//
+
+import SwiftUI
+
+struct GamesSliderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GamesSliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        GamesSliderView()
+    }
+}
